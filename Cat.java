199dd20265704f7d002c1c5fd08f9191a3c56d82
Arazure <bobryk3000@gmail.com>
2022-09-05 15:44:36 +0300

    first commit

---
+++ Cat.java
@@ -0,0 +1,17 @@
+package hm12;
+
+public class Cat {
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+}
